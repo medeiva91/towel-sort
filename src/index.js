b4c112c74bf94feab67e31c2c3d32fc2699741d0
WIN-JR9EEP9V1OJ\medeiva <anastasia.pryakhina@breffi.ru>
2020-10-21 12:35:52 +0200

    feat: desion

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix) {
+    let count = 1;
+    let result = matrix.reduce(function(arr, current){
+      if (count%2 == 0) {
+        current = current.reverse();
+      }
+      arr = arr.concat(current);
+      count++;
+      return arr
+    }, [])
+      return result;
+   } else {
+     return [];
+   }
 }
